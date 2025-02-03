4455d65cc20f5b1693b975da88b7e680f0827aab
f-mantovnai <92172133+f-mantovani@users.noreply.github.com>
2025-02-03 10:57:17 +0100

    update (img format)

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
-import fs from 'fs';
-import axios from 'axios';
-import { glob } from 'glob';
-import { v2 as cloudinary } from 'cloudinary';
-import { pathToFileURL } from 'url';
+import fs from "fs";
+import axios from "axios";
+import { glob } from "glob";
+import { v2 as cloudinary } from "cloudinary";
+import { pathToFileURL } from "url";
 
-
-const dir = await glob('./img/*.*');
+const dir = await glob("./img/*.*");
 
 async function test(fileToUpload) {
-    // Configuration
-    cloudinary.config({
-        cloud_name: process.env.CLOUD_NAME,
-        api_key: process.env.API_KEY,
-        api_secret: process.env.API_SECRET, // Click 'View Credentials' below to copy your API secret
-    });
+  // Configuration
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+  });
 
-    const uploaded = await cloudinary.uploader
-        .upload(fileToUpload, { eager: { transformation: { quality: "50", width: 500,crop: "scale", fetch_format: "webp" } } })
-        .catch((e) => console.log(e));
+  const uploaded = await cloudinary.uploader
+    .upload(fileToUpload, {
+      eager: {
+        transformation: {
+          quality: 100,
+          fetch_format: "webp",
+        },
+      },
+    })
+    .catch((e) => console.log(e));
 
-    console.log(uploaded.eager);
-    return uploaded.eager[0].url;
+  const url = uploaded.eager[0].url;
+  console.log(url);
+  // return
+  const fileName = fileToUpload
+    .split("\\")[1]
+    .split(".")[0]
+    .split("_")
+    .join("_");
+  // .reverse()
+  // return
+  return { url, fileName };
 }
 
-const downloadUrl = await test(dir[0]);
+// const downloadUrl = await test(dir[0]);
 
-async function download(downloadUrl) {
-    const response = await axios.get(downloadUrl, { responseType: 'arraybuffer' }).catch(e =>e.code);
-    const buffer = response.data;
-    fs.writeFile(`./image.webp`, buffer, () => console.log('finished downloading!'));
+async function download(downloadUrl, fileName) {
+  const format = downloadUrl.split('.').at(-1)
+  const response = await axios
+    .get(downloadUrl, { responseType: "arraybuffer" })
+    .catch((e) => e.code);
+  const buffer = response.data;
+  fs.writeFile(`./output/${fileName}.${format}`, buffer, () =>
+    console.log("finished downloading!")
+  );
 }
 
-download(downloadUrl);
+// download(downloadUrl);
+
+for (let i = 0; i < dir.length; i++) {
+  const { url, fileName } = await test(dir[i]);
+  await download(url, fileName);
+}
